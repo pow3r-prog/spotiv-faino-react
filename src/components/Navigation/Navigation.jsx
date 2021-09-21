3066a3c74ff0910c36786558fe067e786e0aeb7d
Vladimir <vova.andreythuk40@gmail.com>
2021-09-21 22:46:23 +0300

    adding icons & updating navigation component

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,14 +1,36 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import './Navigation.scss'
-import logoSvg from '../accents/img/logo.svg'
+import logoIcon from '../accents/img/logo.svg'
+import homeIcon from '../accents/img/homeIcon.svg'
+import libraryIcon from '../accents/img/libraryIcon.svg'
+import favoriteIcon from '../accents/img/favoriteIcon.svg'
 
 const Navigation = () => {
     return ( 
         <div className='container'>
-            <Link style={{ textDecoration: 'none' }} to='/'>
-            <img width='68' src={logoSvg} alt='logo'></img>
-            </Link>
+            <div className='logo'>
+                <Link style={{ textDecoration: 'none' }} to='/'>
+                    <img src={logoIcon} alt='logo'></img>
+                </Link>
+            </div>
+            <ul className='navigation__list'>
+                <li className="navigation__list_item">
+                    <Link to='/'>
+                        <img src={homeIcon} alt='home'></img>
+                    </Link>
+                </li>
+                <li className="navigation__list_item">
+                    <Link to='/'>
+                        <img src={libraryIcon} alt='library'></img>
+                    </Link>
+                </li>
+                <li className="navigation__list_item">
+                    <Link to='/'>
+                            <img src={favoriteIcon} alt='favorite'></img>
+                    </Link>
+                </li>
+            </ul>
         </div>
     )
 }
