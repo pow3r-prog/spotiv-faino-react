3f945c7f58179727468fea4f0490a60352a1913d
Vladimir <vova.andreythuk40@gmail.com>
2021-10-13 00:09:00 +0300

    updating drop-down menu for profile component

--- src/components/Header/Profile.jsx
+++ src/components/Header/Profile.jsx
@@ -1,14 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import userImage from '../accets/img/userImage.jpg'
 import ProfileMenu from './ProfileMenu'
 
 const Profile = () => {
+    const ref = useRef()
     const [open, setOpen] = useState(false)
+    useEffect(() => {
+        const checkIfClickedOutside = e => {
+          if (open && ref.current && !ref.current.contains(e.target)) {
+            setOpen(false)
+          }
+        }
+    
+        document.addEventListener("mousedown", checkIfClickedOutside)
+    
+        return () => {
+          document.removeEventListener("mousedown", checkIfClickedOutside)
+        }
+      }, [open])
     return (
-        <div className='profile' onClick={() => setOpen(!open)}>
+        <div className='profile' onClick={() => setOpen(!open)} ref={ref}>
             <img className='profile__avatar' src={userImage} alt='avatar' />
             <div className='profile__name'>Pow3r</div>
-            {/* {open ? <ProfileMenu /> : false} */}
             {open && <ProfileMenu />}
         </div>
     )
