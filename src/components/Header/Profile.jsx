04cd5fa4f13765f5336dfd6d7324575034665b90
Vladimir <vova.andreythuk40@gmail.com>
2021-10-12 23:47:04 +0300

    updating profile component(adding drop-down menu)

--- src/components/Header/Profile.jsx
+++ src/components/Header/Profile.jsx
@@ -1,56 +1,15 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import Button from '@mui/material/Button'
-import Menu from '@mui/material/Menu'
-import MenuItem from '@mui/material/MenuItem'
-import dropIcon from '../accets/img/dropDownIcon.svg'
 import userImage from '../accets/img/userImage.jpg'
+import ProfileMenu from './ProfileMenu'
 
 const Profile = () => {
-
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const open = Boolean(anchorEl);
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
+    const [open, setOpen] = useState(false)
     return (
-        <div className='profile'>
-            <Button
-                id="basic-button"
-                aria-controls="basic-menu"
-                aria-haspopup="true"
-                aria-expanded={open ? 'true' : undefined}
-                onClick={handleClick}
-            >
-            <img className='profile__avatar' src={userImage} alt="avatar" />
+        <div className='profile' onClick={() => setOpen(!open)}>
+            <img className='profile__avatar' src={userImage} alt='avatar' />
             <div className='profile__name'>Pow3r</div>
-            <img style={{width:'15px', height:'15px'}} src={dropIcon} alt="icon" />
-            </Button>
-            
-            <Menu
-                id="basic-menu"
-                anchorEl={anchorEl}
-                open={open}
-                onClose={handleClose}
-                MenuListProps={{
-                'aria-labelledby': 'basic-button',
-                }}
-            >
-            <div className='profile__links' style={{display:'flex', flexDirection:'column'}}>
-                <Link onClick={handleClose} to='/home'>
-                    home
-                </Link>
-                <Link onClick={handleClose} to='/library'>
-                    library
-                </Link>
-                <Link onClick={handleClose} to='/favorite'>
-                    favorite
-                </Link>
-            </div>
-            </Menu>
+            {/* {open ? <ProfileMenu /> : false} */}
+            {open && <ProfileMenu />}
         </div>
     )
 }
---
+++ src/components/Header/ProfileMenu.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const ProfileMenu = () => {
+    return (
+         <div className='profile__menu'>
+             <div className='menu__list'>
+                    <Link className="menu__list_item" to='/home' style={{color:'#fff'}}>
+                        Profile
+                    </Link>
+                    <Link className="menu__list_item" to='/library' style={{color:'#fff'}}>
+                        Settings
+                    </Link>
+                    <Link className="menu__list_item" to='/favorite' style={{color:'#fff'}}>
+                        Log out
+                    </Link>
+             </div>
+        </div>
+    )
+}
+
+export default ProfileMenu
